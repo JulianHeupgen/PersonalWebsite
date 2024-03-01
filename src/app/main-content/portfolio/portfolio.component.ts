24ac5fffba6ac508172f0b778690a0af30858441
JulianHeupgen <julianheupgen@web.de>
2024-03-01 16:06:25 +0100

    finished portfolio section

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -17,7 +17,7 @@ export class PortfolioComponent {
       title: "Join",
       technologies: ["HTML", "CSS", "JavaScript"],
       description: "Task manager inspired by the Kanban System. Create and organize tasks by using drag and drop functions, assign users and categories.",
-      liveTest_link: "www.google.de",
+      liveTest_link: "https://google.de",
       gitHub_link: "https://github.com/JulianHeupgen/JOIN",
       image: "join.png"
     },
@@ -25,9 +25,17 @@ export class PortfolioComponent {
       title: "El Pollo Loco",
       technologies: ["HTML", "CSS", "JavaScript"],
       description: "A simple Jump N Run Game based on object oriented Programming. Help Pepe to collect coins and fight against chickens.",
-      liveTest_link: "www.google.de",
+      liveTest_link: "https://google.de",
       gitHub_link: "https://github.com/JulianHeupgen/El-Pollo-Loco",
       image: "elpolloloco.png"
+    },
+    {
+      title: "Personal Website",
+      technologies: ["Angular"],
+      description: "My personal homepage. Showing you some of my newest projects. Of course you can test them by clicking on the button below !",
+      liveTest_link: "https://google.de",
+      gitHub_link: "https://github.com/JulianHeupgen/personalWebsite",
+      image: "simpleCRM.png"
     },
 
   ];
