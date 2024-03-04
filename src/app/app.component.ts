3af292028b2671279dffa640b542a51b95a37527
JulianHeupgen <julianheupgen@web.de>
2024-03-04 16:01:34 +0100

    finished private policy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './footer/footer.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainContentComponent, ImprintComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent, ImprintComponent, PrivacyPolicyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
